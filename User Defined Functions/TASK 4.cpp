645156830e9c8cefcde5d9e44f1513a222678b8e
Mohd Jalaluddin Ahmad <9w2tja@gmail.com>
2019-03-13 13:22:20 +0800

    Update TASK 4.cpp

--- User Defined Functions/TASK 4.cpp
+++ User Defined Functions/TASK 4.cpp
@@ -24,10 +24,7 @@ double find_area(double r){
 }
 
 int main(){
-//	double radius = 15.2;
-	double radius;
-	printf("Enter Radius Value : ");
-	scanf("%lf", &radius);
+	double radius = 15.2;
 	printf("Circumference of a circle : %.2lf \n", find_circum(radius));
 	printf("Area of a circle : %.2lf \n", find_area(radius));
 	return 0;
