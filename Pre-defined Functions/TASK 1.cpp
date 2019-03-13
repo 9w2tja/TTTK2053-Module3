f3f0214e781db90c9e62c4e32661d69db389f16c
Mohd Jalaluddin Ahmad <9w2tja@gmail.com>
2019-03-13 13:19:48 +0800

    Update TASK 1.cpp

--- Pre-defined Functions/TASK 1.cpp
+++ Pre-defined Functions/TASK 1.cpp
@@ -7,12 +7,7 @@ double log_any_base (double a, double b){
 }
 
 int main(){
-//	double a=5,b=2;
-	double a,b;
-	printf("Enter 1st log : ");
-	scanf("%lf", &a);
-	printf("Enter 2nd log : ");
-	scanf("%lf", &b);
+	double a=5,b=2;
 	printf("Log1 / Log2 = %.2lf", log_any_base (a,b));
 	return 0;
 }
